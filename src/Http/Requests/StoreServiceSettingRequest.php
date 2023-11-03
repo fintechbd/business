f4be2958ce232c73324e0dace52a276dabb18b74
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-11-04 04:54:50 +0600

    LP-108 service setting store validation done

--- src/Http/Requests/StoreServiceSettingRequest.php
+++ src/Http/Requests/StoreServiceSettingRequest.php
@@ -2,6 +2,7 @@
 
 namespace Fintech\Business\Http\Requests;
 
+use Fintech\Business\Models\ServiceType;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreServiceSettingRequest extends FormRequest
@@ -21,8 +22,17 @@ class StoreServiceSettingRequest extends FormRequest
      */
     public function rules(): array
     {
+        /** @phpstan-ignore-next-line */
+        $service_type_id = (int) collect(request()->segments())->last(); //id of the resource
+        $uniqueRule = 'unique:'.config('fintech.business.service_type_model', ServiceType::class).',service_setting_field_name,'.$service_type_id.',id,service_setting_type,'.$this->input('service_setting_type').',deleted_at,NULL';
+
         return [
-            //
+            'service_setting_type' => ['string', 'required', 'max:255'],
+            'service_setting_name' => ['string', 'required', 'max:255'],
+            'service_setting_field_name' => ['string', 'required', $uniqueRule],
+            'service_setting_type_field' => ['string', 'required'],
+            'service_setting_feature' => ['string', 'required', 'max:255'],
+            'enabled' => ['boolean', 'nullable', 'min:1'],
         ];
     }
 
