4619723d413aa61194c63b28bfd24e82af577313
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-11-04 05:18:34 +0600

    LP-109 service type update validation done

--- src/Http/Requests/UpdateServiceSettingRequest.php
+++ src/Http/Requests/UpdateServiceSettingRequest.php
@@ -2,6 +2,7 @@
 
 namespace Fintech\Business\Http\Requests;
 
+use Fintech\Business\Models\ServiceType;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateServiceSettingRequest extends FormRequest
@@ -33,8 +34,19 @@ class UpdateServiceSettingRequest extends FormRequest
      */
     public function attributes()
     {
+        /** @phpstan-ignore-next-line */
+        $service_type_id = (int) collect(request()->segments())->last(); //id of the resource
+        $uniqueRule = 'unique:'.config('fintech.business.service_type_model', ServiceType::class).',service_type_name,'.$service_type_id.',id,deleted_at,NULL';
+
         return [
-            //
+            'service_type_parent_id' => ['integer', 'nullable'],
+            'service_type_name' => ['string', 'required', 'max:255'],
+            'service_type_is_parent' => ['string', 'required', $uniqueRule],
+            'service_type_step' => ['integer', 'required'],
+            'service_type_data' => ['array', 'required'],
+            'service_type_logo_svg.*' => ['string', 'nullable'],
+            'service_type_logo_png.*' => ['string', 'nullable'],
+            'enabled' => ['boolean', 'nullable', 'min:1'],
         ];
     }
 
