702d3315ffb62f7e90d3dc58d534e3b023e0e8f6
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-11-08 03:18:54 +0600

    LP-109 service type resource done

--- src/Http/Resources/ServiceTypeResource.php
+++ src/Http/Resources/ServiceTypeResource.php
@@ -4,6 +4,19 @@ namespace Fintech\Business\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property integer $service_type_parent_id
+ * @property mixed $serviceTypeParent
+ * @property string $service_type_name
+ * @property string $service_type_slug
+ * @property string $service_type_is_parent
+ * @property string $service_type_is_description
+ * @property integer $service_type_step
+ * @property array $service_type_data
+ * @property boolean $enabled
+ * @property mixed $all_parent_list
+ * @method getKey()
+ */
 class ServiceTypeResource extends JsonResource
 {
     /**
@@ -14,6 +27,21 @@ class ServiceTypeResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $data = [
+          'id' => $this->getKey() ?? null,
+          'service_type_parent_id' => $this->service_type_parent_id ?? null,
+          'service_type_parent_name' => $this->serviceTypeParent->service_type_name ?? null,
+          'service_type_parent_list' => $this->all_parent_list ?? null,
+          'service_type_name' => $this->service_type_name ?? null,
+          'service_type_slug' => $this->service_type_slug ?? null,
+          'service_type_is_parent' => $this->service_type_is_parent ?? null,
+          'service_type_is_description' => $this->service_type_is_description ?? null,
+          'service_type_step' => $this->service_type_step ?? null,
+          'service_type_data' => $this->service_type_data ?? null,
+          'service_type_log_svg' => $this->getMedia('logo_svg') ?? null,
+          'service_type_log_png' => $this->getMedia('logo_png') ?? null,
+          'enabled' => $this->enabled ?? null,
+        ];
+        return $data;
     }
 }
