188f032c091ec97644efd75028a4fb9c919bebb5
mah-shamim <mah-shamim@users.noreply.github.com>
2023-11-07 21:50:33 +0000

    Fix styling

--- src/Http/Resources/ServiceSettingResource.php
+++ src/Http/Resources/ServiceSettingResource.php
@@ -6,25 +6,23 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
- * @property integer $id
+ * @property int $id
  * @property string $service_setting_type
  * @property string $service_setting_name
  * @property string $service_setting_field_name
  * @property string $service_setting_type_field
  * @property string $service_setting_feature
- * @property boolean $enabled
+ * @property bool $enabled
  * @property mixed $links
  * @property mixed $created_at
  * @property mixed $updated_at
+ *
  * @method getKey()
  */
 class ServiceSettingResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
-     *
-     * @param Request $request
-     * @return array
      */
     public function toArray(Request $request): array
     {
