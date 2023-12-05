f781010c8aac97a7e0f592c1aeb523e6bc59af40
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-12-06 01:12:49 +0600

    LP-173 country service tree added

--- src/Facades/Business.php
+++ src/Facades/Business.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Facade;
  * @method static \Fintech\Business\Services\ServicePackageService servicePackage()
  * @method static \Fintech\Business\Services\ServiceSettingService serviceSetting()
  * @method static \Fintech\Business\Services\ServiceStatService serviceStat()
- * @method static \Fintech\Business\Services\ServiceTypeService serviceTYpe()
+ * @method static \Fintech\Business\Services\ServiceTypeService serviceType()
  * @method static \Fintech\Business\Services\ServiceVendorService serviceVendor()
  * @method static \Fintech\Business\Services\ChargeBreakDownService chargeBreakDown()
  * @method static \Fintech\Business\Services\PackageTopChartService PackageTopChart()
