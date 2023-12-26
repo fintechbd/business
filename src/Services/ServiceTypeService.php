a3d83ca9b4eb18555bcb582736a52889d5a0f496
mah-shamim <mah-shamim@users.noreply.github.com>
2023-12-26 22:09:01 +0000

    Fix styling

--- src/Services/ServiceTypeService.php
+++ src/Services/ServiceTypeService.php
@@ -20,76 +20,42 @@ class ServiceTypeService
 
     }
 
-    /**
-     * @param array $filters
-     * @return Collection|Paginator
-     */
     public function list(array $filters = []): Collection|Paginator
     {
         return $this->serviceTypeRepository->list($filters);
 
     }
 
-    /**
-     * @param array $inputs
-     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
-     */
     public function create(array $inputs = []): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->serviceTypeRepository->create($inputs);
     }
 
-    /**
-     * @param $id
-     * @param bool $onlyTrashed
-     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
-     */
     public function find($id, bool $onlyTrashed = false): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->serviceTypeRepository->find($id, $onlyTrashed);
     }
 
-    /**
-     * @param $id
-     * @param array $inputs
-     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
-     */
     public function update($id, array $inputs = []): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->serviceTypeRepository->update($id, $inputs);
     }
 
-    /**
-     * @param $id
-     * @return mixed
-     */
     public function destroy($id): mixed
     {
         return $this->serviceTypeRepository->delete($id);
     }
 
-    /**
-     * @param $id
-     * @return mixed
-     */
     public function restore($id): mixed
     {
         return $this->serviceTypeRepository->restore($id);
     }
 
-    /**
-     * @param array $filters
-     * @return Paginator|Collection
-     */
     public function export(array $filters): Paginator|Collection
     {
         return $this->serviceTypeRepository->list($filters);
     }
 
-    /**
-     * @param array $filters
-     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
-     */
     public function import(array $filters): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->serviceTypeRepository->create($filters);
