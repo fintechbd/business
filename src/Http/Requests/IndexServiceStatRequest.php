b2d1b52aad4bf259689e34760f840713b9c8cd23
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-12-27 05:34:16 +0600

    LP-111 Qualifier can be replaced with an import:22

--- src/Http/Requests/IndexServiceStatRequest.php
+++ src/Http/Requests/IndexServiceStatRequest.php
@@ -2,6 +2,7 @@
 
 namespace Fintech\Business\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class IndexServiceStatRequest extends FormRequest
@@ -19,7 +20,7 @@ class IndexServiceStatRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
