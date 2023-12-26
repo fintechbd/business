fcb0c5ac63ea7cef880a50db9adfc43661cebbdd
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-12-27 04:14:43 +0600

    LP-108 Missing function's return type declaration:45

--- src/Http/Requests/ImportServiceSettingRequest.php
+++ src/Http/Requests/ImportServiceSettingRequest.php
@@ -42,7 +42,7 @@ class ImportServiceSettingRequest extends FormRequest
      *
      * @return array
      */
-    public function attributes()
+    public function attributes(): array
     {
         return [
             //
