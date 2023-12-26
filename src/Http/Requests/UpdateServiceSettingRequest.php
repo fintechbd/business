3f464d23cb20434deb7103e69db37a06250f1190
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-12-27 04:12:48 +0600

    LP-108 Missing function's return type declaration:45

--- src/Http/Requests/UpdateServiceSettingRequest.php
+++ src/Http/Requests/UpdateServiceSettingRequest.php
@@ -42,7 +42,7 @@ class UpdateServiceSettingRequest extends FormRequest
      *
      * @return array
      */
-    public function attributes()
+    public function attributes(): array
     {
         return [
             //
