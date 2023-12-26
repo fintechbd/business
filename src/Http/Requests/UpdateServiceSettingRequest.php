042d3007b952a30b4d4c445931f3b85e0c82b8f3
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-12-27 04:13:14 +0600

    LP-108 Missing function's return type declaration:58

--- src/Http/Requests/UpdateServiceSettingRequest.php
+++ src/Http/Requests/UpdateServiceSettingRequest.php
@@ -55,7 +55,7 @@ class UpdateServiceSettingRequest extends FormRequest
      *
      * @return array
      */
-    public function messages()
+    public function messages(): array
     {
         return [
             //
