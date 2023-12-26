4712543df202947600d14b288b5d4e1d0a06ce47
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-12-27 05:13:03 +0600

    LP-111 Use an instead of 'a' if the following word starts with a vowel sound, e.g. 'an article', 'an hour'.:228

--- src/Http/Controllers/ServiceStatController.php
+++ src/Http/Controllers/ServiceStatController.php
@@ -225,7 +225,7 @@ class ServiceStatController extends Controller
 
     /**
      * @lrd:start
-     * Create a exportable list of the *ServiceStat* resource as document.
+     * Create an exportable list of the *ServiceStat* resource as document.
      * After export job is done system will fire  export completed event
      *
      * @lrd:end
