89592b4d813e8873cc5e48cf3f6224b9f810530f
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-01-25 03:59:56 +0600

    LP-199 add filter

--- src/Repositories/Eloquent/ServiceFieldRepository.php
+++ src/Repositories/Eloquent/ServiceFieldRepository.php
@@ -45,6 +45,10 @@ class ServiceFieldRepository extends EloquentRepository implements InterfacesSer
             }
         }
 
+        if (! empty($filters['service_id'])) {
+                $query->where('service_id', $filters['service_id']);
+        }
+
         //Display Trashed
         if (isset($filters['trashed']) && $filters['trashed'] === true) {
             $query->onlyTrashed();
