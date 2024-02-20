2d85b475bf000a484dba369dcee4da3d13997d3f
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-02-20 23:20:43 +0600

    LP-229 Currency Convert Rate doesn't exists 03

--- src/Http/Controllers/CurrencyRateCalculateController.php
+++ src/Http/Controllers/CurrencyRateCalculateController.php
@@ -29,8 +29,8 @@ class CurrencyRateCalculateController extends Controller
 
         } catch (Exception $exception) {
 
-            return $this->failed("Currency Convert Rate doesn't exists");
-            //throw new \InvalidArgumentException("Currency Convert Rate doesn't exists");
+            //return $this->failed("Currency Convert Rate doesn't exists");
+            throw new \InvalidArgumentException("Currency Convert Rate doesn't exists");
         }
     }
 }
