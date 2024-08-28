9e7150eaf0279320e9958d83e41dc0f0f9a68a53
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-28 17:36:27 +0600

    service field search fixed

--- src/Repositories/Eloquent/ServiceFieldRepository.php
+++ src/Repositories/Eloquent/ServiceFieldRepository.php
@@ -29,30 +29,31 @@ class ServiceFieldRepository extends EloquentRepository implements InterfacesSer
         $query = $this->model->newQuery();
 
         //Searching
-        if (! empty($filters['search'])) {
-            if (is_numeric($filters['search'])) {
-                $query->where($this->model->getKeyName(), 'like', "%{$filters['search']}%");
-            } else {
-                $query->where('name', 'like', "%{$filters['search']}%")
+        if (!empty($filters['search'])) {
+            $query->leftJoin('services', 'services.id', '=', 'service_fields.service_id');
+            $query->where(function ($query) use ($filters) {
+                return $query->where('name', 'like', "%{$filters['search']}%")
                     ->orWhere('label', 'like', "%{$filters['search']}%")
                     ->orWhere('type', 'like', "%{$filters['search']}%")
                     ->orWhere('options', 'like', "%{$filters['search']}%")
                     ->orWhere('value', 'like', "%{$filters['search']}%")
                     ->orWhere('hint', 'like', "%{$filters['search']}%")
                     ->orWhere('validation', 'like', "%{$filters['search']}%")
+                    ->orWhere('services.service_name', 'like', "%{$filters['search']}%")
+                    ->orWhere('services.service_data', 'like', "%{$filters['search']}%")
                     ->orWhere('service_field_data', 'like', "%{$filters['search']}%");
-            }
+            });
         }
 
-        if (! empty($filters['id_not_in'])) {
-            $query->whereNotIn($this->model->getKeyName(), (array) $filters['id_not_in']);
+        if (!empty($filters['id_not_in'])) {
+            $query->whereNotIn($this->model->getKeyName(), (array)$filters['id_not_in']);
         }
 
-        if (! empty($filters['id_in'])) {
-            $query->whereIn($this->model->getKeyName(), (array) $filters['id_in']);
+        if (!empty($filters['id_in'])) {
+            $query->whereIn($this->model->getKeyName(), (array)$filters['id_in']);
         }
 
-        if (! empty($filters['service_id'])) {
+        if (!empty($filters['service_id'])) {
             $query->where('service_id', $filters['service_id']);
         }
 
@@ -60,6 +61,8 @@ class ServiceFieldRepository extends EloquentRepository implements InterfacesSer
         if (isset($filters['trashed']) && $filters['trashed'] === true) {
             $query->onlyTrashed();
         }
+
+        $query->select($this->model->getTable() . '.*');
 
         //Handle Sorting
         $query->orderBy($filters['sort'] ?? $this->model->getKeyName(), $filters['dir'] ?? 'asc');
