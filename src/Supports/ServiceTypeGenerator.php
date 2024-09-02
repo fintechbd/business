65a4d6a94d0c498b778b35e75bba249a34e75c37
hafijul233 <hafijul233@users.noreply.github.com>
2024-09-02 09:26:49 +0000

    Fix styling

--- src/Supports/ServiceTypeGenerator.php
+++ src/Supports/ServiceTypeGenerator.php
@@ -47,12 +47,12 @@ class ServiceTypeGenerator
      */
     public function __construct(array $data, ?int $parentId = null)
     {
-        if (!empty($data['service_type_parent_id']) && $parentId == null) {
+        if (! empty($data['service_type_parent_id']) && $parentId == null) {
             $parentId = $data['service_type_parent_id'];
             unset($data['service_type_parent_id']);
         }
 
-        if (!empty($parentId)) {
+        if (! empty($parentId)) {
             $this->loadParent($parentId);
         }
 
@@ -266,7 +266,7 @@ class ServiceTypeGenerator
     {
         if (file_exists($path) && is_readable($path)) {
             if ($this->verifyImage($path, ['image/svg+xml'])) {
-                $this->logoSvg = 'data:image/svg+xml;base64,' . base64_encode(file_get_contents($path));
+                $this->logoSvg = 'data:image/svg+xml;base64,'.base64_encode(file_get_contents($path));
             } else {
                 throw new \Exception('File is a has invalid mime format');
             }
@@ -281,7 +281,7 @@ class ServiceTypeGenerator
     {
         if (file_exists($path) && is_readable($path)) {
             if ($this->verifyImage($path, ['image/png'])) {
-                $this->logoPng = 'data:image/png;base64,' . base64_encode(file_get_contents($path));
+                $this->logoPng = 'data:image/png;base64,'.base64_encode(file_get_contents($path));
             } else {
                 throw new \Exception('File is a has invalid mime format');
             }
@@ -324,13 +324,13 @@ class ServiceTypeGenerator
     {
         $this->createOrUpdateServiceType();
 
-        if (!$this->hasService) {
+        if (! $this->hasService) {
             return true;
         }
 
-//        $this->createOrUpdateService();
-//
-//        $this->setupServiceStat();
+        //        $this->createOrUpdateService();
+        //
+        //        $this->setupServiceStat();
 
         return true;
     }
