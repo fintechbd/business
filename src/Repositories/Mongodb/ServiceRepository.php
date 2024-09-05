c98e68ce2c6f675986b6953bd2aad3e6e9cc4214
hafijul233 <hafijul233@users.noreply.github.com>
2024-09-05 19:39:01 +0000

    Fix styling

--- src/Repositories/Mongodb/ServiceRepository.php
+++ src/Repositories/Mongodb/ServiceRepository.php
@@ -19,7 +19,7 @@ class ServiceRepository extends MongodbRepository implements InterfacesServiceRe
     {
         $model = app(config('fintech.business.service_model', Service::class));
 
-        if (!$model instanceof Model) {
+        if (! $model instanceof Model) {
             throw new InvalidArgumentException("Mongodb repository require model class to be `MongoDB\Laravel\Eloquent\Model` instance.");
         }
 
@@ -37,7 +37,7 @@ class ServiceRepository extends MongodbRepository implements InterfacesServiceRe
         $query = $this->model->newQuery();
 
         //Searching
-        if (isset($filters['search']) && !empty($filters['search'])) {
+        if (isset($filters['search']) && ! empty($filters['search'])) {
             if (is_numeric($filters['search'])) {
                 $query->where($this->model->getKeyName(), 'like', "%{$filters['search']}%");
             } else {
@@ -46,7 +46,7 @@ class ServiceRepository extends MongodbRepository implements InterfacesServiceRe
         }
 
         //Display Trashed
-        if (isset($filters['trashed']) && !empty($filters['trashed'])) {
+        if (isset($filters['trashed']) && ! empty($filters['trashed'])) {
             $query->onlyTrashed();
         }
 
--- src/Services/ServiceFieldService.php
+++ src/Services/ServiceFieldService.php
@@ -12,9 +12,7 @@ class ServiceFieldService
     /**
      * ServiceFieldService constructor.
      */
-    public function __construct(private readonly ServiceFieldRepository $serviceFieldRepository)
-    {
-    }
+    public function __construct(private readonly ServiceFieldRepository $serviceFieldRepository) {}
 
     public function find($id, $onlyTrashed = false)
     {
