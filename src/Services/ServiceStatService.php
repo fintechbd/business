7bda6edd3674db70d18711c44daa8048b54c1e0d
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-10 11:31:56 +0600

    message issue fixed

--- src/Services/ServiceStatService.php
+++ src/Services/ServiceStatService.php
@@ -195,25 +195,25 @@ class ServiceStatService
 
         if (isset($serviceStatData['lower_limit']) && is_numeric($serviceStatData['lower_limit'])) {
             if ($baseAmount < floatval($serviceStatData['lower_limit'])) {
-                throw new BusinessException(__('business::service_stat.below_lower_limit'));
+                throw new BusinessException(__('business::messages.service_stat.below_lower_limit'));
             }
         }
 
         if (isset($serviceStatData['higher_limit']) && is_numeric($serviceStatData['higher_limit'])) {
             if ($baseAmount > floatval($serviceStatData['higher_limit'])) {
-                throw new BusinessException(__('business::service_stat.upper_limit_exceed'));
+                throw new BusinessException(__('business::messages.service_stat.upper_limit_exceed'));
             }
         }
 
         if (isset($serviceStatData['local_currency_lower_limit']) && is_numeric($serviceStatData['local_currency_lower_limit'])) {
             if ($baseAmount < floatval($serviceStatData['local_currency_lower_limit'])) {
-                throw new BusinessException(__('business::service_stat.local_currency_below_lower_limit'));
+                throw new BusinessException(__('business::messages.service_stat.local_currency_below_lower_limit'));
             }
         }
 
         if (isset($serviceStatData['local_currency_higher_limit']) && is_numeric($serviceStatData['local_currency_higher_limit'])) {
             if ($baseAmount > floatval($serviceStatData['local_currency_higher_limit'])) {
-                throw new BusinessException(__('business::service_stat.local_currency_upper_limit_exceed'));
+                throw new BusinessException(__('business::messages.service_stat.local_currency_upper_limit_exceed'));
             }
         }
 
