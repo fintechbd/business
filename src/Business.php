58b7d6ed25afe38100e0901d127077b1c577b8a1
hafijul233 <hafijul233@users.noreply.github.com>
2024-09-19 20:16:40 +0000

    Fix styling

--- src/Business.php
+++ src/Business.php
@@ -18,59 +18,59 @@ use Fintech\Core\Abstracts\BaseModel;
 class Business
 {
     public function chargeBreakDown($filters = null)
-{
-	return \singleton(ChargeBreakDownService::class, $filters);
+    {
+        return \singleton(ChargeBreakDownService::class, $filters);
     }
 
     public function packageTopChart($filters = null)
-{
-	return \singleton(PackageTopChartService::class, $filters);
+    {
+        return \singleton(PackageTopChartService::class, $filters);
     }
 
     public function service($filters = null)
-{
-	return \singleton(ServiceService::class, $filters);
+    {
+        return \singleton(ServiceService::class, $filters);
     }
 
     public function servicePackage($filters = null)
-{
-	return \singleton(ServicePackageService::class, $filters);
+    {
+        return \singleton(ServicePackageService::class, $filters);
     }
 
     public function serviceSetting($filters = null)
-{
-	return \singleton(ServiceSettingService::class, $filters);
+    {
+        return \singleton(ServiceSettingService::class, $filters);
     }
 
     public function serviceStat($filters = null)
-{
-	return \singleton(ServiceStatService::class, $filters);
+    {
+        return \singleton(ServiceStatService::class, $filters);
     }
 
     public function serviceType($filters = null)
-{
-	return \singleton(ServiceTypeService::class, $filters);
+    {
+        return \singleton(ServiceTypeService::class, $filters);
     }
 
     public function serviceVendor($filters = null)
-{
-	return \singleton(ServiceVendorService::class, $filters);
+    {
+        return \singleton(ServiceVendorService::class, $filters);
     }
 
     /**
      * @return CurrencyRateService
      */
     public function currencyRate($filters = null)
-{
-	return \singleton(CurrencyRateService::class, $filters);
+    {
+        return \singleton(CurrencyRateService::class, $filters);
     }
 
     /**
      * @return ServiceFieldService
      */
     public function serviceField($filters = null)
-{
-	return \singleton(ServiceFieldService::class, $filters);
+    {
+        return \singleton(ServiceFieldService::class, $filters);
     }
 
     public function serviceTypeManager(array $attributes = [], int|BaseModel|string|null $parent = null): ServiceTypeGenerator
