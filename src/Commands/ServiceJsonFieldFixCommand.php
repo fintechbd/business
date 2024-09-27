7a2797c287f8f75bcf66e22ff4f1ca9152d01777
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-27 12:17:39 +0600

    command added

---
+++ src/Commands/ServiceJsonFieldFixCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Fintech\Business\Commands;
+
+use Fintech\Business\Facades\Business;
+use Fintech\Core\Traits\HasCoreSetting;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class ServiceJsonFieldFixCommand extends Command
+{
+    use HasCoreSetting;
+
+    public $signature = 'business:service-json-field-fix';
+
+    public $description = 'Run a casting program to fix json field issue';
+
+    private string $module = 'Business';
+
+    /**
+     * @throws \Throwable
+     */
+    public function handle(): int
+    {
+        $services = Business::service()->list();
+
+        foreach ($services as $service) {
+            $this->task("Fixing <fg=blue>{$service->service_name}</> service casting issue", function () use (&$service) {
+                $serviceData = $service->service_data;
+
+                $service->save();
+            });
+        }
+
+        return self::SUCCESS;
+    }
+}
