0497059945131252debd23c192813ac380a60a92
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-10-10 03:04:05 +0600

    local balance display added

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -11,9 +11,9 @@ return [
         'service_assigned' => 'Assigning services to :vendor vendor successful.',
     ],
     'service_stat' => [
-        'below_lower_limit' => 'Below minimum limit! Please adjust the transaction amount.',
-        'upper_limit_exceed' => 'Maximum limit exceeded! Please adjust the transaction amount.',
-        'local_currency_below_lower_limit' => 'Local currency below minimum limit! Please adjust the transaction amount.',
-        'local_currency_upper_limit_exceed' => 'Local currency maximum limit exceeded! Please adjust the transaction amount.',
+        'below_lower_limit' => 'Amount is less than :currency minimum limit! Use amount above then :min_amount.',
+        'upper_limit_exceed' => 'Amount is greater than :currency maximum limit! Use amount less then :max_amount.',
+        'local_currency_below_lower_limit' => 'Amount is less than transferred :currency minimum limit! Use amount above then :min_amount.',
+        'local_currency_upper_limit_exceed' => 'Amount is greater than maximum transferred :currency limit exceeded! Use amount less then :max_amount.',
     ],
 ];
